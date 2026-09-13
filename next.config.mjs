68310ae777a599a7c541b2c1dbb5de90da7dfdd9
Yassine <yzekhnini56@gmail.com>
2026-09-13 22:55:50 +0100

    Ignore typescript build errors in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  typescript: {
+    // Allows production builds to successfully complete even if project has type errors
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
